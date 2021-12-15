fb795990deb0a0f987045904d8fedd6a48c9b1dd
deepak.singhh <deepakshivam1998@gmail.com>
2021-12-15 13:06:46 +0530

    added

--- share-json/index.js
+++ share-json/index.js
@@ -5,7 +5,7 @@ dotenv.config()
 
 //MIDDILWARES
 const app = express();
-app.use(express.json({limit: '50mb'}))
+app.use(express.json({limit: '100mb'}))
 
 // Routes
 const router = require('./routes')
